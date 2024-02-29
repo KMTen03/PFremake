3ee15106f3490faf5ece5559115ededae98a329b
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-02-29 15:06:04 +0000

    ポートフォリオのリメイク

---
+++ db/migrate/20240228144228_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      #タイトル
+      t.string :title, null: false
+      #コンテンツ
+      t.string :content, null :false
+      #学習時間
+      t.string :time, null :false
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20240228144233_create_post_tags.rb
@@ -0,0 +1,13 @@
+class CreatePostTags < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_tags do |t|
+      #Postと関連付ける
+      #references型を使用して予想外のidが入らないようにする。
+      t.references :post, foreign_key: true
+      #Tagと関連付けるためのカラム
+      #references型を使用して予想外のidが入らないようにする。
+      t.references :tag, foreign_key: true
+      t.timestamps
+    end
+  end
+end
