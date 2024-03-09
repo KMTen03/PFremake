888801d7e7d0ca34073dfea014835cc5537814b3
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-03-09 15:20:28 +0000

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  
+  devise_for :users, controllers: {
+    registrations: 'publics/registrations',
+    sessions: 'publics/sessions',
+    passwords: 'users/passwords'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
