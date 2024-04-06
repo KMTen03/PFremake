cab7152a94434fba409dfa5095139bbe6186e5f4
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-04-06 15:06:30 +0000

    ゲストユーザー作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_one_attached :profile_image
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :introduce, length: { maximum: 200 }
+  
+  def self.guest
+    find_or_created_by!(email: "guest@example.com") do |guest|
+      guest.password = SecureRandom.urlsafe_base64
+      guest.name = "ゲスト"
+      guest.is_deleted = false
+    end
+  end
+  
 end
