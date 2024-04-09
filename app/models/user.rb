8d080eb507e75e8ea2406bc8238ad7b2ca92edca
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-04-09 14:57:24 +0000

    ユーザーモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_one_attached :profile_image
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :introduce, length: { maximum: 200 }
-  
+
   def self.guest
     find_or_created_by!(email: "guest@example.com") do |guest|
       guest.password = SecureRandom.urlsafe_base64
@@ -18,5 +18,20 @@ class User < ApplicationRecord
       guest.is_deleted = false
     end
   end
-  
+
+  def self.ranking
+    find(UserPost.group(:user_id).order('count(post_id) desc').limit(10).pluck(:user_id))
+  end
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/profile.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg',content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
