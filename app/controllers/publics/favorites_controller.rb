feb873d1a5c69265ea56329d3af60f7ab9075ea1
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-05-27 11:55:19 +0000

    いいね機能

--- app/controllers/publics/favorites_controller.rb
+++ app/controllers/publics/favorites_controller.rb
@@ -1,7 +1,13 @@
 class Publics::FavoritesController < ApplicationController
   def create
+    @post_like = Like.new(end_user_id: current_end_user.id, post_id: params[:post_id])
+    @post_like.save
+    redirect_to post_path(params[:post_id]) 
   end
 
   def destroy
+    @post_like = Like.find_by(end_user_id: current_end_user.id, post_id: params[:post_id])
+    @post_like.destroy
+    redirect_to post_path(params[:post_id])
   end
 end
