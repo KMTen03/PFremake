b76f5ed48591be843de47d5b32b07a4b0d914b7e
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-05-29 15:54:52 +0000

    モデル名変更予定

--- app/controllers/publics/favorites_controller.rb
+++ app/controllers/publics/favorites_controller.rb
@@ -1,13 +1,13 @@
 class Publics::FavoritesController < ApplicationController
   def create
-    @post_like = Like.new(end_user_id: current_end_user.id, post_id: params[:post_id])
-    @post_like.save
+    @post_favorite = Favorite.new(user_id: current_user.id, post_id: params[:post_id])
+    @post_favorite.save
     redirect_to post_path(params[:post_id]) 
   end
 
   def destroy
-    @post_like = Like.find_by(end_user_id: current_end_user.id, post_id: params[:post_id])
-    @post_like.destroy
+    @post_favorite = favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
+    @post_favorite.destroy
     redirect_to post_path(params[:post_id])
   end
 end
