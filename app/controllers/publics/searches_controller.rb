4325b0599a493165b0a769faa53486a4f7d8f035
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-07-07 16:27:12 +0000

    検索機能追加

--- app/controllers/publics/searches_controller.rb
+++ app/controllers/publics/searches_controller.rb
@@ -1,2 +1,14 @@
 class Publics::SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def search
+    @range = params[:range]
+    @word = params[:word]
+    
+    if @range == "User"
+      @posts = Post.looks(params[:search], params[:word])
+    else
+      @tags = Tag.looks(params[:search], params[:word])
+    end
+  end
 end
