df26c20f1384786da329cc46c1a09996b66eb025
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2024-07-10 13:38:15 +0000

    コメント機能コントローラー

--- app/controllers/publics/comments_controller.rb
+++ app/controllers/publics/comments_controller.rb
@@ -1,2 +1,27 @@
 class Publics::CommentsController < ApplicationController
+   before_action :authenticate_end_user!
+  
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @post = @comment.post
+      @comments = @post.comments.includes(:user)
+      render "posts/show"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment.destroy
+    redirect_to post_path(@comment.post)
+  end
+  
+private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(end_user_id: current_end_user.id, post_id: params[:post_id])
+  end
+
 end
